549d4ba063f6ca47984d22d790d503fe6e79338f
fishertom <tfisher92@gmail.com>
2023-03-17 17:03:18 +0000

    change landing layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import "../styles/hero.css";
-
+import Bio from "./Bio";
+import { useLocation } from "react-router-dom";
 function Hero() {
+  const location = useLocation();
+
+  console.log(location.pathname);
   return (
     <div id="hero">
-      <div className="title-box">
-        <h1 id="hero-title">Tom Fisher</h1>
-        <h3>Developer - Designer</h3>
+      <div className="title-portrait">
+        <div id="headshot-box">
+          <div className="star"></div>
+          <img
+            id="headshot"
+            src={require("../images/pxl.png")}
+            alt="Tom Fisher"
+          />
+        </div>
+        <div className="title-box">
+          <h1 id="hero-title">Tom Fisher</h1>
+          <h3>Developer - Designer</h3>
+        </div>
       </div>
+      {location.pathname === "/" ? <Bio /> : <></>}
     </div>
   );
 }
